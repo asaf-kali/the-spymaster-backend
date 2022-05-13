af1e8dfe3d5158bb33d482ae9ba579119f1ab44d
Asaf Kali <akali93@gmail.com>
2022-05-13 15:17:38 +0300

    Add `SessionId` class.

--- telegram_bot/the_spymaster_bot.py
+++ telegram_bot/the_spymaster_bot.py
@@ -81,6 +81,13 @@ class BotState(IntEnum):
     Playing = auto()
 
 
+class SessionId(BaseModel):
+    chat_id: int
+
+    def __hash__(self):
+        return hash(self.json())
+
+
 class Session(BaseModel):
     game_id: Optional[int]
     state: Optional[GameState]
@@ -132,8 +139,12 @@ class EventHandler:
         return self.update.effective_chat.id if self.update.effective_chat else None
 
     @property
+    def session_id(self) -> SessionId:
+        return SessionId(chat_id=self.chat_id)  # type: ignore
+
+    @property
     def session(self) -> Optional[Session]:
-        return self.bot.sessions.get(self.user.id)
+        return self.bot.get_session(session_id=self.session_id)
 
     @property
     def state(self) -> Optional[GameState]:
@@ -326,7 +337,7 @@ class StartEventHandler(EventHandler):
         request = StartGameRequest(language=session_config.language)
         response = self.client.start_game(request)
         session = Session(game_id=response.game_id, state=response.game_state, config=session_config)
-        self.bot.set_session(self.user.id, session=session)
+        self.bot.set_session(session_id=self.session_id, session=session)
         self.remove_keyboard()
         self.send_markdown(f"Game *#{response.game_id}* is starting! 🥳")
         return self.fast_forward()
@@ -363,7 +374,7 @@ class CustomHandler(EventHandler):
     def handle(self):
         game_config = GameConfig()
         session = Session(config=game_config)
-        self.bot.set_session(self.user.id, session=session)
+        self.bot.set_session(session_id=self.session_id, session=session)
         keyboard = build_language_keyboard()
         self.send_text("🌍 Pick language:", reply_markup=keyboard)
         return BotState.ConfigLanguage
@@ -403,8 +414,8 @@ class GetSessionsHandler(EventHandler):
     def handle(self):
         log.info(f"Getting sessions for user {self.user.full_name}")
         sessions_dict = {}
-        for user_id, session in self.bot.sessions.items():
-            sessions_dict[user_id] = session.clean_dict()
+        for session_id, session in self.bot.sessions.items():
+            sessions_dict[session_id.chat_id] = session.clean_dict()
         pretty_json = json.dumps(sessions_dict, indent=2, ensure_ascii=False)
         self.send_text(pretty_json)
 
@@ -457,13 +468,16 @@ Use '-pass' and '-quit' to pass the turn and quit the game.
 
 class TheSpymasterBot:
     def __init__(self, base_backend: str = None):
-        self.sessions: Dict[int, Session] = {}
+        self.sessions: Dict[SessionId, Session] = {}
         self.client = TheSpymasterClient(base_backend=base_backend)
 
-    def set_session(self, user_id: int, session: Optional[Session]):
+    def set_session(self, session_id: SessionId, session: Optional[Session]):
         if not session:
-            self.sessions.pop(user_id, None)
-        self.sessions[user_id] = session
+            self.sessions.pop(session_id, None)
+        self.sessions[session_id] = session  # type: ignore
+
+    def get_session(self, session_id: SessionId) -> Optional[Session]:
+        return self.sessions.get(session_id)
 
     def generate_handler(self, handler_type: Type[EventHandler]):
         return handler_type.handler(self)
