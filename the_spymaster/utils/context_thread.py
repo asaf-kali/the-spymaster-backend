30038da3ee717caf4607d4fe6ecbb2b06477b25c
Asaf Kali <akali93@gmail.com>
2022-06-06 23:20:15 +0300

    Context thread fix.

--- the_spymaster/utils/context_thread.py
+++ the_spymaster/utils/context_thread.py
@@ -6,7 +6,7 @@ from the_spymaster.utils import get_logger
 log = get_logger(__name__)
 
 
-# Hacky way to make all threads inherit log context from theirs creating thread
+# Hacky way to make all threads inherit log context from theirs parent thread
 
 
 def _make_target_context_aware(target: Optional[Callable], parent_context: dict) -> Optional[Callable]:
@@ -23,10 +23,10 @@ def _make_target_context_aware(target: Optional[Callable], parent_context: dict)
     return wrapper
 
 
-_original_thread_init = threading.Thread.__init__  # type: ignore
+original_thread_init = threading.Thread.__init__  # type: ignore
 
 
-def context_aware_init(  # noqa
+def context_aware_thread_init(  # noqa
     self,
     group=None,
     target: Optional[Callable] = None,
@@ -36,9 +36,9 @@ def context_aware_init(  # noqa
     kwargs: Optional[Mapping[str, Any]] = None,
 ):
     context_aware_target = _make_target_context_aware(target=target, parent_context=log.context)
-    _original_thread_init(
+    original_thread_init(
         self, group=group, target=context_aware_target, name=name, args=args or [], kwargs=kwargs, daemon=daemon
     )
 
 
-threading.Thread.__init__ = context_aware_init  # type: ignore
+threading.Thread.__init__ = context_aware_thread_init  # type: ignore
