3b9c68a77411e177009a3b1775aec8209f25aeb3
Asaf Kali <akali93@gmail.com>
2022-07-02 18:49:24 +0300

    Add DNS record to API.

---
+++ tf_service/dns.tf
@@ -0,0 +1,31 @@
+resource "aws_apigatewayv2_domain_name" "api_domain_name" {
+  domain_name = local.backend_domain
+
+  domain_name_configuration {
+    certificate_arn = local.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "api_dns_record" {
+  name    = aws_apigatewayv2_domain_name.api_domain_name.domain_name
+  type    = "A"
+  zone_id = local.hosted_zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.api_domain_name.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.api_domain_name.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "api_mapping" {
+  api_id      = local.api_gateway_id
+  domain_name = aws_apigatewayv2_domain_name.api_domain_name.id
+  stage       = var.env
+}
+
+output "api_endpoint_url" {
+  value = "https://${local.backend_domain}"
+}
