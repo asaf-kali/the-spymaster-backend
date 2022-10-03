00c4d9e7f0519dc20182e7d9c8b8948f88668f4c
Asaf Kali <akali93@gmail.com>
2022-10-04 00:15:22 +0300

    Testing: add moto mocking for dynamodb.

--- src/api/tests/spymaster_test.py
+++ src/api/tests/spymaster_test.py
@@ -1,13 +1,27 @@
 from django.test import TestCase, override_settings
+from moto.dynamodb import mock_dynamodb
 
-from api.management.commands.dev_init import create_admin
-from api.models.user import SpymasterUser
+from api.logic.db import GameItem
+from the_spymaster.config import get_config
 
 
+@override_settings(DEBUG=True)
 class SpymasterTest(TestCase):
     @classmethod
-    @override_settings(DEBUG=True)
     def setUpClass(cls) -> None:
         super().setUpClass()
+        cls.config = get_config()
+        cls.mock_dynamo = mock_dynamodb()
+        cls.mock_dynamo.start()
+        GameItem.create_table()
+
+        from api.management.commands.dev_init import create_admin
+        from api.models.user import SpymasterUser
+
         create_admin()
         cls.admin = SpymasterUser.objects.get(username="admin")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.mock_dynamo.stop()
+        super().tearDownClass()
