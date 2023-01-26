6eb3ef9c4b2d18ecd01d5216ac00d515c29bc021
Asaf Kali <akali93@gmail.com>
2023-01-27 01:00:01 +0200

    SpymasterExceptionHandlerMiddleware: support error_code in response.

--- src/api/middleware.py
+++ src/api/middleware.py
@@ -11,7 +11,11 @@ from django.http.response import HttpResponseBase
 from django.utils.deprecation import MiddlewareMixin
 from requests import HTTPError
 from rest_framework import status
-from the_spymaster_util.http_client import CONTEXT_ID_HEADER_KEY, extract_context
+from the_spymaster_util.http_client import (
+    CONTEXT_ID_HEADER_KEY,
+    extract_context,
+    get_error_code,
+)
 from the_spymaster_util.logger import get_logger, wrap
 from the_spymaster_util.measure_time import MeasureTime
 
@@ -23,32 +27,37 @@ log = get_logger(__name__)
 class SpymasterExceptionHandlerMiddleware(MiddlewareMixin):
     def process_exception(self, request: WSGIRequest, exception: Exception):
         log.debug("Processing exception: %s", exception, exc_info=True)
+        error_code = get_error_code(exception)
         if isinstance(exception, ValidationError):
-            return JsonResponse({"message": str(exception)}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"message": str(exception), "error_code": error_code}, status=status.HTTP_400_BAD_REQUEST
+            )
         if isinstance(exception, BadRequestError):
             return JsonResponse(
-                {"message": exception.message, "details": exception.details}, status=exception.status_code
+                {"message": exception.message, "error_code": error_code, "details": exception.details},
+                status=exception.status_code,
             )
         if isinstance(exception, PermissionDenied):
             log.warning(f"User {wrap(request.user)} can't access {wrap(request.path)}!")
             raise exception
         if isinstance(exception, GameRuleError):
             return JsonResponse(
-                {"message": "Invalid move", "details": str(exception)}, status=status.HTTP_400_BAD_REQUEST
+                {"message": "Invalid move", "error_code": error_code, "details": str(exception)},
+                status=status.HTTP_400_BAD_REQUEST,
             )
         if isinstance(exception, HTTPError) and exception.response is not None:
             response = exception.response
             try:
                 data = response.json()
             except:  # noqa
-                data = {"message": "Internal server error", "details": response.text}
+                data = {"message": "Internal server error", "error_code": error_code, "details": response.text}
             return JsonResponse(data=data, status=response.status_code)
 
         sentry_sdk.capture_exception(exception)
         log.exception("Uncaught exception")
         sentry_sdk.flush(timeout=5)
         message = "Internal server error" if not settings.DEBUG else str(exception)
-        data = {"message": message, "context_id": log.context_id}
+        data = {"message": message, "error_code": error_code, "context_id": log.context_id}
         if settings.DEBUG:
             data["details"] = traceback.format_exc()
         headers = {CONTEXT_ID_HEADER_KEY: log.context_id}
