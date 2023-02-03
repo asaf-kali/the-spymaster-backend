d84978b5edd63a2150e2ec050ef864f450d27c96
Asaf Kali <akali93@gmail.com>
2023-02-03 23:58:29 +0200

    Replace service KMS key with account KMS key.

--- tf/main.tf
+++ tf/main.tf
@@ -32,10 +32,10 @@ locals {
   }
   env               = local.env_map[terraform.workspace]
   # Base
-  project_name   = "the-spymaster-backend"
-  service_name   = "${local.project_name}-${local.env}"
-  aws_account_id = data.aws_caller_identity.current.account_id
-  project_root   = "${path.module}/.."
+  project_name      = "the-spymaster-backend"
+  service_name      = "${local.project_name}-${local.env}"
+  aws_account_id    = data.aws_caller_identity.current.account_id
+  project_root      = "${path.module}/.."
   # Domain
   base_app_domain   = "the-spymaster.xyz"
   hosted_zone_id    = "Z0770508EK6R7V32364I"
@@ -45,8 +45,10 @@ locals {
     "staging" = ""
     "prod"    = ""
   }
-  domain_suffix  = local.domain_suffix_map[local.env]
-  backend_domain = "backend.${local.domain_suffix}${local.base_app_domain}"
+  domain_suffix   = local.domain_suffix_map[local.env]
+  backend_domain  = "backend.${local.domain_suffix}${local.base_app_domain}"
+  # Encryption
+  default_key_arn = "arn:aws:kms:us-east-1:096386908883:key/0b9c713c-1c4b-43ad-84df-1f62117838f0"
 }
 
 data "aws_caller_identity" "current" {}
--- tf/security.tf
+++ tf/security.tf
@@ -1,17 +1,17 @@
-resource "aws_kms_key" "kms_key" {
-  description = "KMS key for backend service."
-}
-
-resource "aws_kms_alias" "kms_key_alias" {
-  name          = "alias/${local.service_name}-key"
-  target_key_id = aws_kms_key.kms_key.id
-}
+#resource "aws_kms_key" "kms_key" {
+#  description = "KMS key for backend service."
+#}
+#
+#resource "aws_kms_alias" "kms_key_alias" {
+#  name          = "alias/${local.service_name}-key"
+#  target_key_id = aws_kms_key.kms_key.id
+#}
 
 resource "aws_ssm_parameter" "sentry_dsn" {
   name   = "${local.service_name}-sentry-dsn"
   type   = "SecureString"
   value  = var.sentry_dsn
-  key_id = aws_kms_key.kms_key.arn
+  key_id = local.default_key_arn
 }
 
 resource "random_password" "django_secret_key" {
